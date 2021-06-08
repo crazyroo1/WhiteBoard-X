cd25a27cab856d51201d8cc0a60e5f5c46589717
Turner Eison <turner@turnereison.com>
2021-06-08 17:10:54 -0400

    Initial Commit

---
+++ WhiteBoard X/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhiteBoard X
+//
+//  Created by Turner Eison on 6/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhiteBoard X/WhiteBoard_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteBoard_XApp.swift
+//  WhiteBoard X
+//
+//  Created by Turner Eison on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WhiteBoard_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
